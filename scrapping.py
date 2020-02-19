1ab8f2d2bfdfaf7bc2763f856e26ee40dfc02aea
levistan <42348846+arjun619@users.noreply.github.com>
2020-02-19 14:52:29 +0530

    data science and sports scrapper functional

--- scrapping.py
+++ scrapping.py
@@ -3,6 +3,23 @@ import requests
 
 def data_science_scrap():
     raw_info=requests.get("https://towardsdatascience.com/data-science/home")
-    info=BeautifulSoup(raw_info.text)
+    info=BeautifulSoup(raw_info.text,"html.parser")
+    temp=info.find_all('a',"u-block u-xs-height170 u-height172 u-backgroundSizeCover u-backgroundOriginBorderBox u-backgroundColorGrayLight u-borderLighter")
+    content_links=[]
+    for i in range(len(temp)):
+        url=temp[i]["href"]
+        article=temp[i].text
+        content_links.append([article,url])
+    return content_links    
+def football_scrap():
+    raw_info=requests.get("https://www.foxsportsasia.com/football/premier-league/")
+    info=BeautifulSoup(raw_info.text,"html.parser")
     temp=info.find_all('a')
-    
+    content_links=[]
+    for link in range(7):
+        if temp[link].text!='':
+            url=temp[link]['href']
+            content=temp[link].text
+            content_links.append([content,url])
+    return content_links
+
