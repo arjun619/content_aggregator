e7969194534a9ba7834826eb9ecd6ae6cad48ced
levistan <42348846+arjun619@users.noreply.github.com>
2020-02-29 21:36:30 +0530

    interface updated

--- interface.py
+++ interface.py
@@ -7,18 +7,22 @@ flag=input("Do you want to enter the content aggregator ")
 if flag[0]=="N" or flag=="n":
     exit()
 
-utility_funcs.check_new_user()
+user_data=utility_funcs.check_new_user()
 utility_funcs.menu()
 interest=int(input())
 while (interest!=0):
     if interest==1 :
         data=scrapping.data_science_scrap()
-        utility_funcs.link_display(data)
+        utility_funcs.link_display(user_data,data)
     elif interest==2:
         data=scrapping.football_scrap()
-        utility_funcs.link_display(data)
+        utility_funcs.link_display(user_data,data)
     utility_funcs.menu()
     interest=int(input())
+
+data_management.view_history(user_data.name)
+
+
 
     
     
